32aefd3b5f2c4fd119436b4158ef3eee58cc9078
Mariana Lungu <lun.mariancik@gmail.com>
2022-12-11 11:12:50 -0500

    remove commented code

--- js/about.js
+++ js/about.js
@@ -12,30 +12,3 @@ acc[i].addEventListener("click", function() {
     }
 });
 }
-
-
-// function myFunction() {
-//   var x = document.getElementById("panel");
-//   if (x.style.display === "none") {
-//     x.style.display = "block";
-//   } else {
-//     x.style.display = "none";
-//   }
-// }
-
-
-// function myFunction() {
-//   var dots = document.getElementById("dots");
-//   var moreText = document.getElementById("more");
-//   var btnText = document.getElementById("myBtn");
-
-//   if (dots.style.display === "none") {
-//     dots.style.display = "inline";
-//     btnText.innerHTML = "Read more"; 
-//     moreText.style.display = "none";
-//   } else {
-//     dots.style.display = "none";
-//     btnText.innerHTML = "Read less"; 
-//     moreText.style.display = "inline";
-//   }
-// }
